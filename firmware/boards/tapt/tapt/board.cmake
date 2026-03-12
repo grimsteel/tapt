033908226b4f672844af04770c00cc4993f2fc27
Siddhant Kameswar <115331356+grimsteel@users.noreply.github.com>
2026-03-11 20:50:26 -0500

    firmware: board config, build config, prj.conf

---
+++ firmware/boards/tapt/tapt/board.cmake
@@ -0,0 +1,14 @@
+# Copyright (c) 2021 Nordic Semiconductor ASA
+# SPDX-License-Identifier: Apache-2.0
+
+board_runner_args(jlink "--device=nrf52" "--speed=4000")
+board_runner_args(pyocd "--target=nrf52810" "--frequency=4000000")
+
+set(OPENOCD_NRF5_SUBFAMILY "nrf52")
+
+include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/openocd-nrf5.board.cmake)
+board_set_sim_runner_ifnset(example)
+board_finalize_runner_args(example) # No default arguments to provide.
